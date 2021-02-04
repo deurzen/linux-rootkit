f34236640594648fceaea94eb5d760eb061c5156
Tizian Leonhardt <tizianleonhardt@web.de>
2021-02-04 22:45:23 +0100

    Make occ.sh smarter

--- project/occ.sh
+++ project/occ.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Extract all occurences of function calls and the assigned variables from kernel sources
-funcs=("kmalloc" "kzalloc")
+funcs=("kmalloc" "kzalloc" "vmalloc" "vzalloc")
 out=".funcs"
 
 rm -f cscope.out cscope.files $out
@@ -20,13 +20,18 @@ if ! [ -x "$(command -v cscope)" ]; then
 fi
 
 echo "Generating file cscope.files.."
-find  $1 \
+
+old_pwd=$PWD
+
+cd $1
+find  . \
     -name "*.[chxsS]" -print > ./cscope.files
 echo "Done!"
 
 echo "Generating occurence database.."
-echo "$1" >> $out
 for f in ${funcs[@]}; do
     cscope -L -0 $f >> $out
 done
 echo "Done!"
+
+mv $out $old_pwd
